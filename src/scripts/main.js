2c9a95c31527e10dfcd13a78acfcc2a31f1fe912
N-UX539 <spidereye539@gmail.com>
2026-09-15 17:54:29 +0600

    -navigatio-bar _01

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
-import '../css/index.css'; 
+import "../css/index.css";
+
+// navMenu Toogler Function
+let navMenu = document.getElementById("navLinks");
+let toggleBtn = document.getElementById("toggleBtn");
+function toggleState() {
+  navMenu.classList.toggle("max-lg:translate-x-0");
+}
+toggleBtn.addEventListener("click", toggleState);
+toggleBtn.addEventListener("blur", () =>
+  navMenu.classList.remove("max-lg:translate-x-0"),
+);
